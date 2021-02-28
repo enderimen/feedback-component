389c52d1122155e940c622d90381abf58bfb3249
Ender Imen <eimen@boynergrup.com>
2021-03-01 00:39:17 +0300

    When voting, it has been made to show the comment field for feedback.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 // import another component
 import main from './main';
 
+const rateMessage = document.querySelector(".m-rate__messageArea");
+const rateButtonList = document.querySelectorAll(".js-rate input");
+
+rateButtonList.forEach(button => {
+    button.addEventListener('click', (event) => {
+        if(event.target.checked) {
+            rateMessage.style.display = "block";
+            rateMessage.classList.add("-opened");
+        }
+    });
+});
+
 main();
