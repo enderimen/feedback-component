3fa37ffe1d58c504a9b18496c45453459bfb8956
Ender Imen <eimen@boynergrup.com>
2021-03-02 01:16:17 +0300

    Sentiment rating has been completed with pure js and css.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 // import another component
 import main from './main';
 
+// Number Rate
 const rateMessage = document.querySelector(".m-rate__messageArea");
 const rateButtonList = document.querySelectorAll(".js-rate input");
+
+// Sentiment Rate
+const sentimentRateMessage = document.querySelector(".m-sentiment__messageArea");
+const sentimentButtonList = document.querySelectorAll(".js-sentimentRate input");
 
 rateButtonList.forEach(button => {
     button.addEventListener('click', (event) => {
         if(event.target.checked) {
             rateMessage.style.display = "block";
             rateMessage.classList.add("-opened");
+        }
+    });
+});
+
+sentimentButtonList.forEach(button => {
+    button.addEventListener('click', (event) => {
+        if(event.target.checked) {
+            sentimentRateMessage.classList.add("-show");
         }
     });
 });
