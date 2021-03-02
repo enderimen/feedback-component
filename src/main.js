ced6aabd70d9fab9818d750d0f876caffbf7b4db
Ender Imen <eimen@boynergrup.com>
2021-03-03 00:10:48 +0300

    Sentiment rate card has been completed.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
 export default () => {
-    
+    // Number Rate
+    const rateMessage = document.querySelector(".m-rate__messageArea");
+    const rateButtonList = document.querySelectorAll(".js-rate input");
+
+    // Sentiment Rate
+    const sentimentRateMessage = document.querySelector(".m-sentiment__messageArea");
+    const sentimentButtonList = document.querySelectorAll(".js-sentimentRate input");
+
+    rateButtonList.forEach(button => {
+        button.addEventListener('click', (event) => {
+            if(event.target.checked) {
+                rateMessage.style.display = "block";
+                rateMessage.classList.add("-opened");
+            }
+        });
+    });
+
+    sentimentButtonList.forEach(button => {
+        button.addEventListener('click', (event) => {
+            if(event.target.checked) {
+                sentimentRateMessage.classList.add("-show");
+            }
+        });
+    });
 };
